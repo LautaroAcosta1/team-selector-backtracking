622eaea6542989dda28414b939b287af6acc9f78
LautaroAcosta1 <acostalauty765@gmail.com>
2026-06-07 23:01:06 -0300

    Se conecta el primer boton y se empiezan a guardar los datos que ingrese el usuario.

--- src/ui/MainFrame.java
+++ src/ui/MainFrame.java
@@ -3,12 +3,23 @@ package ui;
 import javax.swing.*;
 import java.awt.*;
 
+import modelo.Persona;
+import modelo.Rol;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainFrame {
 
     private JFrame frame;
     private JTextArea txtResultado;
 
+    private List<Persona> personas;
+
     public MainFrame() {
+
+        personas = new ArrayList<>();
+
         initialize();
     }
 
@@ -26,6 +37,8 @@ public class MainFrame {
 
         JButton btnPersona =
                 new JButton("Agregar Persona");
+
+        btnPersona.addActionListener(e -> agregarPersona());
 
         JButton btnIncompatibilidad =
                 new JButton("Agregar Incompatibilidad");
@@ -55,5 +68,74 @@ public class MainFrame {
 
     public void mostrar() {
         frame.setVisible(true);
+    }
+
+    private void agregarPersona() {
+
+        String nombre =
+                JOptionPane.showInputDialog(
+                        frame,
+                        "Nombre:"
+                );
+
+        if (nombre == null || nombre.isBlank()) {
+            return;
+        }
+
+        Rol rol = (Rol) JOptionPane.showInputDialog(
+                frame,
+                "Rol:",
+                "Seleccionar Rol",
+                JOptionPane.QUESTION_MESSAGE,
+                null,
+                Rol.values(),
+                Rol.LIDER
+        );
+
+        if (rol == null) {
+            return;
+        }
+
+        String textoCalificacion =
+                JOptionPane.showInputDialog(
+                        frame,
+                        "Calificación:"
+                );
+
+        if (textoCalificacion == null) {
+            return;
+        }
+
+        int calificacion;
+
+        try {
+
+            calificacion =
+                    Integer.parseInt(textoCalificacion);
+
+        } catch (NumberFormatException e) {
+
+            JOptionPane.showMessageDialog(
+                    frame,
+                    "La calificación debe ser un número."
+            );
+
+            return;
+        }
+
+        Persona persona =
+                new Persona(
+                        nombre,
+                        rol,
+                        calificacion
+                );
+
+        personas.add(persona);
+
+        txtResultado.append(
+                "Persona agregada: "
+                        + persona
+                        + "\n"
+        );
     }
 }
